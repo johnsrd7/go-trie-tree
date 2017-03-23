1382a1ab3b5e897107684c48ff23655396b7c655
Robert Johnson <johnsrd7@gmail.com>
2017-03-22 21:47:19 -0700

    Add initial implementation for trie tree.

---
+++ trie.go
@@ -0,0 +1,104 @@
+package trie
+
+// trieNode is an internal node for the trie tree.
+type trieNode struct {
+	val      rune
+	clean    bool
+	children map[rune]*trieNode
+}
+
+// Tree is a trie tree ADT that holds words of an alphabet.
+type Tree struct {
+	root           *trieNode
+	specialEndRune rune
+}
+
+// newTrieNode creates a new trieNode with the given value and
+// the given clean bit set.
+func newTrieNode(val rune, clean bool) *trieNode {
+	return &trieNode{val, clean, make(map[rune]*trieNode)}
+}
+
+// NewTrieTree creates a new to a TrieTree that keeps the given
+// specialEndRune as a special character that cannot be used in
+// words given to the tree.
+func NewTrieTree(specialEndRune rune) *Tree {
+	return &Tree{newTrieNode(specialEndRune, true), specialEndRune}
+}
+
+// Add adds the given word to the tree.
+func (t Tree) Add(word string) bool {
+	if len(word) == 0 {
+		return true
+	}
+
+	curNode := t.root
+	undo := false
+	for _, c := range word {
+		if c == t.specialEndRune {
+			// The word had the special end rune, so
+			// we need to break here and then undo any
+			// dirty nodes that were added.
+			undo = true
+			break
+		}
+
+		// First, we need to see if the char is in the map
+		if _, ok := curNode.children[c]; !ok {
+			curNode.children[c] = &trieNode{c, false, make(map[rune]*trieNode)}
+		}
+
+		// Next, set the curNode to the matching one in children
+		// and "recurse" with it.
+		curNode = curNode.children[c]
+	}
+
+	if undo {
+		curNode := t.root
+		for _, c := range word {
+			if c == t.specialEndRune {
+				if curNode.children[c] != nil {
+					// Set it back to nil if it got messed up.
+					curNode.children[c] = nil
+				}
+
+				break
+			}
+
+			if !curNode.children[c].clean {
+				// We have found our dirty path,
+				// so we can just delete that node
+				// from the child and rid the whole
+				// dirty path.
+				delete(curNode.children, c)
+				break
+			}
+
+			curNode = curNode.children[c]
+		}
+
+		return false
+	}
+
+	// At the end, we have the final node, so we need to set the
+	// ending char so we know that we have a word.
+	if _, ok := curNode.children[t.specialEndRune]; ok {
+		// If we are here, then the word already exists,
+		// so we don't need to do anything because the word
+		// already exists.
+		return false
+	}
+
+	// If we got here, then the path for this word doesn't already
+	// exist, so we need to add the end rune.
+	curNode.children[t.specialEndRune] = nil
+
+	// At this point, we just need to set all the nodes to clean
+	curNode = t.root
+	for _, c := range word {
+		curNode = curNode.children[c]
+		curNode.clean = true
+	}
+
+	return true
+}
---
+++ trie_test.go
@@ -0,0 +1,128 @@
+package trie
+
+import (
+	"testing"
+	"unicode/utf8"
+)
+
+func TestNewTrieNode(t *testing.T) {
+	for r := rune(0); r < utf8.MaxRune; r++ {
+		clean := int32(r)%2 == 0
+		tn := newTrieNode(r, clean)
+
+		if tn.val != r {
+			t.Errorf("trieNode.val Error: Expected: %v, Actual: %v", r, tn.val)
+		}
+		if tn.clean != clean {
+			t.Errorf("trieNode.clean Error: Expected: %b, Actual: %b", clean, tn.clean)
+		}
+		if tn.children == nil {
+			t.Error("trieNode.children Error: Should not be nil.")
+		}
+		if len(tn.children) != 0 {
+			t.Errorf("trieNode.children Error: Children map should be empty, actual value is %d", len(tn.children))
+		}
+	}
+}
+
+func TestNewTrieTree(t *testing.T) {
+	endRunes := []rune{' ', '*', 'a', '$', '\n', 'r'}
+
+	for _, er := range endRunes {
+		tt := NewTrieTree(er)
+
+		if tt.specialEndRune != er {
+			t.Errorf("TrieTree.specialEndRune Error: Expected: %v, Actual: %v", er, tt.specialEndRune)
+		}
+
+		if tt.root.val != er {
+			t.Errorf("TrieTree.root.val Error: Expected: %v, Actual: %v", er, tt.root.val)
+		}
+		if !tt.root.clean {
+			t.Error("TrieTree.root.clean Error: Root should always be clean")
+		}
+		if tt.root.children == nil {
+			t.Error("TrieTree.trieNode.children Error: should not be nil.")
+		}
+		if len(tt.root.children) != 0 {
+			t.Errorf("TrieTree.trieNode.children Error: Children map should be empty, actual value is %d", len(tt.root.children))
+		}
+	}
+}
+
+func TestAdd(t *testing.T) {
+	tt := NewTrieTree('*')
+
+	if !tt.Add("") {
+		t.Error("Add returned false for word with 0 length")
+	}
+
+	words := []string{"Robert", "Bad", "wold", "abcdefghijklmnopqrstuvwxyz"}
+	for _, word := range words {
+		if !tt.Add(word) {
+			t.Errorf("Failed to add word %s to TrieTree", word)
+		}
+
+		// Now we need to check that it was added
+		n := tt.root
+		for c := 0; c < len(word); c++ {
+			r := rune(word[c])
+			if _, ok := n.children[r]; !ok {
+				t.Error("Char %v was not added to tree", c)
+			}
+
+			n = n.children[r]
+		}
+
+		if _, ok := n.children['*']; !ok {
+			t.Error("SpecialEndRune was not put at the end of the word")
+		}
+	}
+
+	// Check that we get false on re-add of words
+	for _, word := range words {
+		if tt.Add(word) {
+			t.Errorf("Word %s was already added, should get false back", word)
+		}
+	}
+
+	// Check that word with special end rune returns false
+	if tt.Add("test*word") {
+		t.Error("Should get false back for words with special end rune in it")
+	}
+	// Now check that the tree is still clean
+	if !checkTrieNodeIsClean(tt.root, tt.specialEndRune) {
+		t.Error("Tree has been left in a dirty state after an invalid word added")
+	}
+
+	// Check that word with special end rune returns false
+	tt.Add("test")
+	if tt.Add("test*") {
+		t.Error("Should get false back for words with special end rune in it")
+	}
+	// Now check that the tree is still clean
+	if !checkTrieNodeIsClean(tt.root, tt.specialEndRune) {
+		t.Error("Tree has been left in a dirty state after an invalid word added")
+	}
+}
+
+func checkTrieNodeIsClean(tn *trieNode, specialEndRune rune) bool {
+	for cr, ctn := range tn.children {
+		if cr == specialEndRune {
+			if ctn != nil {
+				return false
+			}
+			continue
+		}
+
+		if !ctn.clean {
+			return false
+		}
+
+		if !checkTrieNodeIsClean(ctn, specialEndRune) {
+			return false
+		}
+	}
+
+	return true
+}
